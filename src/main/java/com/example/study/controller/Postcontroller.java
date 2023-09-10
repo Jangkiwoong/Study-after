6798f11e9fca011b8b9b76cb350230bcbde753b1
Jangkiwoong <zoo11111@gmail.com>
2023-09-10 18:52:02 +0900

    [docs] : docs

--- src/main/java/com/example/study/controller/Postcontroller.java
+++ src/main/java/com/example/study/controller/Postcontroller.java
@@ -21,21 +21,27 @@ public class Postcontroller {
     public ResponseEntity<Message> createPost(@RequestBody PostRequestDto postRequestDto) {
         return postService.createPost(postRequestDto);
     }
+
     //게시물 조회
     @GetMapping("/posts")
     public ResponseEntity<Message> readPost(@RequestParam(defaultValue = "0") int pageNumber,
                                             @RequestParam(defaultValue = "10") int pageSize) {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         return postService.readPost(pageable);
+        /**
+          http://localhost:8080/api/posts?pageNumber=0&pageSize=4
+         **/
     }
-    /**
-     게시물 상세조회
-     http://localhost:8080/api/post-param?id=1&title=장기웅
-     **/
+    
+    //게시물 상세조회
     @GetMapping("/post-param")
     public ResponseEntity<Message> readParamPost(@RequestParam Long id, @RequestParam String title) {
         return postService.readParamPost(id, title);
+        /**
+         http://localhost:8080/api/post-param?id=1&title=장기웅
+         **/
     }
+
     //게시물 수정
    @PatchMapping("/post/{postId}")
     public ResponseEntity<Message> updatePost(@PathVariable Long postId, @RequestBody PostRequestDto postRequestDto) {
