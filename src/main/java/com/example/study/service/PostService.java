26d5944bccc1af8dc5f45602ff5f9a6327b358b8
Jangkiwoong <zoo11111@gmail.com>
2023-09-12 20:57:52 +0900

    asdsadadasd

--- src/main/java/com/example/study/service/PostService.java
+++ src/main/java/com/example/study/service/PostService.java
@@ -5,7 +5,6 @@ import com.example.study.dto.PostResponseDto;
 import com.example.study.entity.Post;
 import com.example.study.global.util.Message;
 import com.example.study.repository.PostRepository;
-import com.example.study.repository.queryDsl.CustomPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
