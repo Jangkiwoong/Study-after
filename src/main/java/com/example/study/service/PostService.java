3fff3ed3cf1e0d02651633239c6c10a5de686f98
Jangkiwoong <zoo11111@gmail.com>
2023-10-02 16:53:47 +0900

    [Refactor] : refactoring createfile

--- src/main/java/com/example/study/service/PostService.java
+++ src/main/java/com/example/study/service/PostService.java
@@ -39,6 +39,7 @@ public class PostService {
     }
 
     //게시물 조회 (페이지네이션)
+    @Transactional
     public  ResponseEntity<Message> readPost(Pageable pageable) {
         Page<Post> postPage = postRepository.findAll(pageable);
         if (postPage.isEmpty()) {
@@ -52,6 +53,7 @@ public class PostService {
     }
 
     //게시물 상세조회
+    @Transactional
     public ResponseEntity<Message> readParamPost(Long id, String title) {
         Post post = postRepository.findByIdAndTitle(id, title).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
@@ -60,6 +62,7 @@ public class PostService {
     }
 
     //게시물 수정
+    @Transactional
     public ResponseEntity<Message> updatePost(Long postID, PostRequestDto postRequestDto) {
        Post post = postRepository.findById(postID).orElseThrow(
                () -> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
@@ -70,6 +73,7 @@ public class PostService {
     }
 
     //게시물 삭제
+    @Transactional
     public ResponseEntity<Message> deletePost(Long postID) {
         postRepository.findById(postID).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
@@ -79,6 +83,7 @@ public class PostService {
     }
 
     //이미지 추가
+    @Transactional
     public ResponseEntity<Message> createImagePost(PostRequestDto postRequestDto) throws IOException {
         String imgUrl = s3Uploader.upload(postRequestDto.getImage());
         Post post = new Post(imgUrl, postRequestDto);
@@ -87,22 +92,32 @@ public class PostService {
         return new ResponseEntity<>(new Message("이미지 등록 성공", null), HttpStatus.CREATED);
     }
 
+    //파일 업로드
+    @Transactional
     public ResponseEntity<Message> createfile(MultipartFile multipartFile) throws IOException {
+        // 현재 프로젝트 디렉토리의 경로를 가져옴 (현재 생성되는 디렉토리의 경로 + 파일을 저장할 경로)
+        // System.getProperty("user.dir")은 자바에서 현재 실행중인 디렉토리 위치를 알 수 있다.
         String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
 
+        // 파일 이름에 UUID를 추가하여 중복을 피함
         UUID uuid = UUID.randomUUID();
-
         String fileName = uuid + "_" + multipartFile.getOriginalFilename();
+
+        // 파일의 상대 경로를 생성 (클라이언트가 파일을 다운로드 할때 필요한 경로)
         String filePath =  "/files/" + fileName;
 
+        // 파일을 저장할 위치와 이름을 설정합니다
         File saveFile = new File(projectPath, fileName);
 
+        // 업로드된 파일의 내용을 실제 파일로 복사하여 저장합니다
         multipartFile.transferTo(saveFile);
 
+        // 업로드된 파일 정보를 데이터베이스에 저장하기 위한 PostFile 객체 생성
         PostFile postFile = new PostFile();
         postFile.setFileName(fileName);
         postFile.setFilePath(filePath);
 
+        // PostFile 객체를 데이터베이스에 저장합니다
         postFileRepository.save(postFile);
         return new ResponseEntity<>(new Message("파일 저장 성공", multipartFile.getOriginalFilename()), HttpStatus.OK);
     }
